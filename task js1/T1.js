48d2c6c0a459288c02d64bf948692732ef80b90a
ahmednaser09 <ahmedalshamilh@gmail.com>
2025-01-04 21:41:47 +0300

    Add files via upload

---
+++ task js1/T1.js
@@ -0,0 +1,38 @@
+var y=1000,x=500;
+document.write('Cash flow ratio=');
+document.write( (y/x) +"<br>");
+
+
+document.write(' Net income=');
+document.write(y-x +"<br>");
+
+document.write(' Total assets=');
+document.write(y+x +"<br>");
+
+document.write(' Net income (using profit margin and sales)=');
+document.write(y*x +"<br>");
+
+
+
+var a=7, b=9, c=2;
+document.write(' Average=');
+document.write( (a+b+c)/3 +"<br>");
+
+var p=150 ;
+document.write(' Discount');
+document.write(p  - (p)*0.30 +"<br>");
+
+
+var age=20;
+ var num= age > 18 && age < 30
+document.write(' Age=');
+document.write(num +"<br>");
+
+
+document.write (Math.pow(2, 3)+"<br>")
+
+document.write (10%4)
+
+
+
+
